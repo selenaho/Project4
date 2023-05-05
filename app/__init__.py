dab82afba955ffaa7393e9d6548cf1c1a68c4467
James Yu <jyu30@stuy.edu>
2023-05-05 14:03:06 -0400

    flask stuff' ''

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def main_page():
-    return render_template("game.html")
+    if request.method == 'GET':
+        return render_template("game.html")#pass through the four answer choices, id of the correct answer 
+    else:
+        return redirect(url_for("result"))#pass through correct answer and 
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    if request.method == 'GET':
+        return render_template("result.html")
+    else:
+        return redirect(url_for("main_page"))
+
 
 if __name__ == "__main__":
     app.debug = True
