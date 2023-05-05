7ae64a67288c1ae1337a212e2b4263a19de57a56
Sadi Nirloy <snirloy30@stuy.edu>
2023-05-05 14:06:16 -0400

    LSKJFOIWHREO:GHO

--- app/utl/tableHandler.py
+++ app/utl/tableHandler.py
@@ -4,3 +4,20 @@ Silly Ramen Obliterates Frog
 
 import sqlite3
 import csv
+import os
+
+def makeTables():
+    return 0
+    
+def loadElection():
+    election = open(os.getcwd() + "/../csvs/president_county_candidate.csv", "r")
+    fullText = election.readlines()
+    for row in fullText:
+    #    print(row)
+        if (row != fullText[0]):
+            line = row.split("<")
+        #Here is where we need to start the Database stuff
+
+
+
+loadElection()
