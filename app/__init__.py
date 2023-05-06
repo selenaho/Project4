6d6e82985dfafe6d8084a45b4cf4903915a455b8
Selena Ho <sho30@stuy.edu>
2023-05-06 14:17:11 -0400

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
     if request.method == 'GET':
-        return render_template("game.html")#pass through the four answer choices, id of the correct answer 
+        #need db method to get an array [choice a, choice b, choice c, choice d, index of correct choice, county, state]
+        return render_template("game.html")#pass through the four answer choices and index of the correct answer 
     else:
-        return redirect(url_for("result"))#pass through correct answer and 
+        return redirect(url_for("result"))#pass through correct answer, county name, state name
 
 @app.route("/result", methods=['GET', 'POST'])
 def result():
