734b401ed33535ffc6f0ae286e24391cf0d476e2
Selena Ho <sho30@stuy.edu>
2023-05-10 18:05:15 -0400

    flaks

--- app/__init__.py
+++ app/__init__.py
@@ -14,16 +14,16 @@ def main_page():
     if request.method == 'GET':
         randState = random.choice(table.getStates())
         randCounty = random.choice(table.getCountyfromState(randState))
-        winner = table.countyWin(randState, randCounty)
-        winnerIndex = random.randInt(0,3)
+        winner = table.countyWin(randState, randCounty)[0]
+        winnerIndex = random.randint(0,3)
         candidateArray = [0] * 4
         candidateArray[winnerIndex] = winner
         for i in range(len(candidateArray)):
             if candidateArray[i] == 0:
-                candidateArray[i] = random.choice(table.getCandidates())
+                candidateArray[i] = random.choice(table.getCandidates())[0]
         print(candidateArray)
 
-        return render_template("game.html")#pass through the four answer choices and index of the correct answer 
+        return render_template("game.html", countyName = randCounty, stateName = randState, a = candidateArray[0], b = candidateArray[1], c = candidateArray[2], d = candidateArray[3], winnerIndex = winnerIndex)#pass through the four answer choices and index of the correct answer 
     else:
        return redirect(url_for("result"))#pass through correct answer, county name, state name
 
