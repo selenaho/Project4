e9b2578082b374bdf3a58c26f2b8f2f9f2a6a97c
Selena Ho <sho30@stuy.edu>
2023-05-10 19:02:46 -0400

    more flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 import random
 import os
@@ -14,7 +14,10 @@ def main_page():
     if request.method == 'GET':
         randState = random.choice(table.getStates())
         randCounty = random.choice(table.getCountyfromState(randState))
+        print(randState)
+        print(randCounty)
         winner = table.countyWin(randState, randCounty)[0]
+        print(winner)
         winnerIndex = random.randint(0,3)
         candidateArray = [0] * 4
         candidateArray[winnerIndex] = winner
@@ -25,12 +28,28 @@ def main_page():
 
         return render_template("game.html", countyName = randCounty, stateName = randState, a = candidateArray[0], b = candidateArray[1], c = candidateArray[2], d = candidateArray[3], winnerIndex = winnerIndex)#pass through the four answer choices and index of the correct answer 
     else:
-       return redirect(url_for("result"))#pass through correct answer, county name, state name
+       county = request.form.get("county") #gets hidden value of county name from form
+       state = request.form.get("state") #gets hidden value of state name from form
+       #winner = table.countyWin(state, county)
+       #print(table.countyWin(state, county))
+       #print(winner)
+       print(state)
+       print(county)
+       return redirect(url_for("result", state=state, county=county))#pass through correct answer, county name, state name
 
-@app.route("/result", methods=['GET', 'POST'])
-def result():
+@app.route("/result/<state>/<county>", methods=['GET', 'POST'])
+def result(state, county):
     if request.method == 'GET':
-        return render_template("result.html")
+        path = request.path.split("/")
+        #path[2] is state
+        state = path[2]
+        #path[3] is county
+        county = path[3]
+        print(path)
+        print(state)
+        print(county)
+        winner = table.countyWin(state, county)[0]
+        return render_template("result.html", countyName = county, stateName = state)
     else:
         return redirect(url_for("main_page"))
 
