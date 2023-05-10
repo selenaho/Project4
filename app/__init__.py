d29ad3f494885035111e8ac29f04cee424f8f9ad
SNirloy <yolrinidas@gmail.com>
2023-05-10 00:51:12 -0400

    Worst way to handle pathing

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, request
 import requests
 import random
+import os
 
 import utl.tableHandler as table
 
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
+    base = app.root_path
+    table.loadElection(base)
+    
     if request.method == 'GET':
-        randState = random.choice(table.getStates())
-        randCounty = random.choice(table.getCountyfromState(randState))
-        winner = table.countyWin(randState, randCounty)
+        randState = random.choice(table.getStates(base))
+        randCounty = random.choice(table.getCountyfromState(base, randState))
+        winner = table.countyWin(base, randState, randCounty)
         winnerIndex = random.randInt(0,3)
         candidateArray = [0] * 4
         candidateArray[winnerIndex] = winner
         for i in range(len(candidateArray)):
             if candidateArray[i] == 0:
-                candidateArray[i] = random.choice(table.getCandidates)
+                candidateArray[i] = random.choice(table.getCandidates(base))
         print(candidateArray)
-                
 
         return render_template("game.html")#pass through the four answer choices and index of the correct answer 
     else:
-        return redirect(url_for("result"))#pass through correct answer, county name, state name
+       return redirect(url_for("result"))#pass through correct answer, county name, state name
 
 @app.route("/result", methods=['GET', 'POST'])
 def result():
