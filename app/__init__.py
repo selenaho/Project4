50eccd4633a49da14c20a47986215749d2b5646e
Selena Ho <sho30@stuy.edu>
2023-05-11 21:41:33 -0400

    yup

--- app/__init__.py
+++ app/__init__.py
@@ -43,19 +43,45 @@ def main_page():
 
         return render_template("game.html", countyName = randCounty, stateName = randState, stateNameValue = stateNameValue, countyNameValue = countyNameValue, a = choiceArray[0], b = choiceArray[1], c = choiceArray[2], d = choiceArray[3], winnerIndex = winnerIndex)#pass through the four answer choices and index of the correct answer 
     else:
-       county = request.form.get("county") #gets hidden value of county name from form
-       state = request.form.get("state") #gets hidden value of state name from form
-       #winner = table.countyWin(state, county)
-       #print(table.countyWin(state, county))
-       #print(winner)
-       #print(state)
-       #print(county)
-       #state = state.replace(" ", "|")
-       #county = county.replace(" ", "|")
-       #print("________")
-       #print(state)
-       #print(county)
-       return redirect(url_for("result", state=state, county=county))#pass through correct answer, county name, state name
+        
+        county = request.form.get("county") #gets hidden value of county name from form
+        state = request.form.get("state") #gets hidden value of state name from form
+        #winner = table.countyWin(state, county)
+        #print(table.countyWin(state, county))
+        #print(winner)
+        #print(state)
+        #print(county)
+        #state = state.replace(" ", "|")
+        #county = county.replace(" ", "|")
+        #print("________")
+        #print(state)
+        #print(county)
+        winnerIndex = request.form.get("winnerIndex")
+        won = False
+        #validate_on_submit() method of the form returns True when the form was submitted and the data was accepted by all the field validators
+        print(type(winnerIndex))
+        if 'a' in request.form: #if user pressed on button choice a
+            print("presssed")
+            print(winnerIndex)
+            
+            if winnerIndex == "0":
+                print("0 yup")
+                won = True
+        elif 'b' in request.form:
+            if winnerIndex == "1":
+                won = True
+        elif 'c' in request.form:
+            if winnerIndex == "2":
+                won = True
+        elif 'd' in request.form:
+            if winnerIndex == "3":
+                won = True
+        print(won)
+        if won:
+            bool="y"
+        else:
+            bool="n"
+        return redirect(url_for("result", state=state, county=county, bool=bool))#pass through correct answer, county name, state name
 
 #function to help make sure the answer choices have no repeats
 def removeRepeat(array, choice):
@@ -67,8 +93,8 @@ def removeRepeat(array, choice):
             print(array)
             break
 
-@app.route("/result/<state>/<county>", methods=['GET', 'POST'])
-def result(state, county):
+@app.route("/result/<state>/<county>/<bool>", methods=['GET', 'POST'])
+def result(state, county, bool):
     if request.method == 'GET':
         path = request.path.split("/")
         #path[2] is state
@@ -76,16 +102,26 @@ def result(state, county):
         #path[3] is county
         countyName = path[3].replace("|", " ")
         print(path)
-        print(stateName)
-        print(countyName)
-        winner = table.countyWin(stateName, countyName)[0]
-
-        #html for hidden unemployment value: <input type="hidden" name="unemploymentData" id="job_data" value={{job_data}}>
+        #print(stateName)
+        #print(countyName)
         
+        #selecting win/loss message
+        won = path[4]
+        message = ""
+        winMessages = ["Congrats!", "Good job!", "You passed gov!"]
+        loseMessages = ["Wrong :(", "Not quite", "Close enough I guess", "You certainly tried!"]
+        if won == "y":
+            message = random.choice(winMessages)
+        if won == "n":
+            message = random.choice(loseMessages)
+        
+        winner = table.countyWin(stateName, countyName)[0]
+        #html for hidden unemployment value: <input type="hidden" name="unemploymentData" id="job_data" value={{job_data}}>
+
         #html for hidden education value: <input type="hidden" name="educationData" id="edu_data" value={{edu_data}}>
 
         #pass the data into the render template through here using stateName and countyName to get data first
-        return render_template("result.html", countyName = countyName, stateName = stateName, winner = winner)
+        return render_template("result.html", countyName = countyName, stateName = stateName, winner = winner, message=message)
     else:
         return redirect(url_for("main_page"))
 
